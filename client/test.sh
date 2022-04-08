a65f36c55ae82e92e664a191a8ba46de607ed79b
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-04-08 16:45:38 +0800

    更新client/test.sh

--- client/test.sh
+++ client/test.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
+id=$(date +%s)
 echo test write
-time dd if=/dev/zero of=/c3sm/hello.test.txt bs=1000M count=1
+time dd if=/dev/zero of=/c3sm/hello.test.txt.$id bs=1000M count=1
 
 echo test read
-time dd of=/dev/zero if=/c3sm/hello.test.txt bs=1000M count=1
-
-echo clean
-time rm /c3sm/hello.test.txt
+time dd of=/dev/zero if=/c3sm/hello.test.txt.$id bs=1000M count=1
