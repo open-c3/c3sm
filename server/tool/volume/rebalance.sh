c85eab5d7decc09b8b3a6b865a2806523ed793af
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-04-08 18:52:50 +0800

    修复rebalance.sh

--- server/tool/volume/rebalance.sh
+++ server/tool/volume/rebalance.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
 ctrl=$1
-
 if [ "X$ctrl" = "X" ];then
-    echo $0 start|stop|status
+    echo "$0 start|stop|status"
     exit
 fi
-
 docker exec -it openc3-c3sm gluster volume rebalance c3sm_distributed $ctrl
